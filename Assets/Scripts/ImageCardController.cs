e36446b38dd4d86d01208f3d43d49b6c4f00c7f0
henohenon <takeru.hawk.son@gmail.com>
2024-08-13 08:36:33 +0900

    change color

--- Assets/Scripts/ImageCardController.cs
+++ Assets/Scripts/ImageCardController.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class ImageCardController : MonoBehaviour, ISelectable
 {
+    private SpriteRenderer _spriteRenderer;
+    
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
     public void OnHover()
     {
-        Debug.Log("OnHover");
+        _spriteRenderer.color = Color.gray;
     }
 
     public void OnSelect()
     {
-        Debug.Log("OnSelect");
+        _spriteRenderer.color = Color.red;
     }
 
     public void OnUnhover()
     {
-        Debug.Log("OnUnhover");
+        _spriteRenderer.color = Color.white;
     }
 }
